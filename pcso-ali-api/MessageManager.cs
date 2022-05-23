07b0850758316de9a13bf5c8321049fb1f0796c7
Ali <alifarahnak>
2022-05-23 07:29:20 +0000

    Get and Post messages working

---
+++ pcso-ali-api/MessageManager.cs
@@ -0,0 +1,11 @@
+﻿namespace pcso_ali_api;
+
+    public class MessageManager
+    {
+        public List<Message> Messages = new List<Message> ();
+
+        public MessageManager()
+        {
+
+        }
+    }
--- pcso-ali-api/Program.cs
+++ pcso-ali-api/Program.cs
@@ -1,4 +1,6 @@
 
+using pcso_ali_api;
+
 var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
@@ -8,13 +10,16 @@ var app = builder.Build();
     app.UseSwaggerUI();
     app.UseHttpsRedirection();
 
+    MessageManager messageManager = new MessageManager();
+
     app.MapGet("Hello", () =>  "This is very cool I hope");
     app.MapGet("Product", () => "Get some Product from Azure SQL");
     
-    app.MapPost("Product", (Product p) => "Product saved in SQL");
+    app.MapPost("Product", (Product p) => "");
     app.MapDelete("Product", (string name) => "Deleted");
 
-    app.MapPost("Chat", (Message msg) => "Message posted to chat SQL");
+    app.MapGet("Chat", () => messageManager?.Messages);
+    app.MapPost("Chat", (Message msg) => messageManager?.Messages?.Add(msg));
 
 app.Run();
 
